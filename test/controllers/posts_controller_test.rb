52b4e07c6befc185f7352a32c19fa281f9673602
Sado4 <derasado04@gmail.com>
2024-08-23 17:11:18 +0900

    feat: test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+    test "postsのindexページが表示される" do
+    get posts_url
+    assert_response :success
+  end
 end
